7205e32bda5e0b7f005d9d12f372a4ad608a3045
Fulbabu <fulbabucse@gmail.com>
2022-11-05 21:19:24 +0600

    home product add

--- src/pages/Home/Products/Products.jsx
+++ src/pages/Home/Products/Products.jsx
@@ -13,6 +13,16 @@ const Products = () => {
   }, []);
   return (
     <div>
+      <div className="flex flex-col items-center w-full mb-5">
+        <h3 className="text-4xl font-bold text-gray-600">Just For You</h3>
+        <p className="text-gray-700 w-full lg:w-3/5 font-semibold">
+          At Ornato.com bangladesh, we carry some of the finest brands and offer
+          amazing quality that is hard to find elsewhere under one roof. So wait
+          no more! Choose from list of men’s clothing brands in bangladesh using
+          our brand filter and find the product that best matches your styling
+          taste and budget needs.
+        </p>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {products.map((product) => (
           <Product key={product.id} product={product}></Product>
